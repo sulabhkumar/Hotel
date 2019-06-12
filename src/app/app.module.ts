707e11a65e5c7ab0a4f95ab034589ee2bc3eaa51
Sulabh <sulabhkumar0@gmil.com>
2019-06-12 11:48:23 +0530

    Bsic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import {DishService} from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule
 
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
