871b683cbcf374a29e5e910b976864a400cefc30
Sulabh <sulabhkumar0@gmil.com>
2019-06-18 11:04:22 +0530

    Single page Application Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,6 +9,7 @@ import { MenuComponent } from '../menu/menu.component';
 export const routes:Routes =[
 {path:'home',component:HomeComponent},
 {path:'menu',component: MenuComponent},
+{ path: 'dishdetail/:id',     component: DishdetailComponent },
 {path: '',redirectTo:'/home',pathMatch:'full'},
 {path:'contactus',component:ContactComponent}
 ];
